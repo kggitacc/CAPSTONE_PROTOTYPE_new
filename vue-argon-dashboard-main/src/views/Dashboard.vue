7414bc425b7f9da33ddadb8453fdf0a3d771e961
kggitacc <gadkekeeley@gmail.com>
2022-06-17 00:56:49 +1000

    big push

---
+++ vue-argon-dashboard-main/src/views/Dashboard.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="py-4 container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 col-12">
+            <card
+              :title="stats.money.title"
+              :value="stats.money.value"
+              :percentage="stats.money.percentage"
+              :iconClass="stats.money.iconClass"
+              :iconBackground="stats.money.iconBackground"
+              :detail="stats.money.detail"
+              directionReverse
+            ></card>
+          </div>
+          <div class="col-lg-3 col-md-6 col-12">
+            <card
+              :title="stats.users.title"
+              :value="stats.users.value"
+              :percentage="stats.users.percentage"
+              :iconClass="stats.users.iconClass"
+              :iconBackground="stats.users.iconBackground"
+              :detail="stats.users.detail"
+              directionReverse
+            ></card>
+          </div>
+          <div class="col-lg-3 col-md-6 col-12">
+            <card
+              :title="stats.clients.title"
+              :value="stats.clients.value"
+              :percentage="stats.clients.percentage"
+              :iconClass="stats.clients.iconClass"
+              :iconBackground="stats.clients.iconBackground"
+              :percentageColor="stats.clients.percentageColor"
+              :detail="stats.clients.detail"
+              directionReverse
+            ></card>
+          </div>
+          <div class="col-lg-3 col-md-6 col-12">
+            <card
+              :title="stats.sales.title"
+              :value="stats.sales.value"
+              :percentage="stats.sales.percentage"
+              :iconClass="stats.sales.iconClass"
+              :iconBackground="stats.sales.iconBackground"
+              :detail="stats.sales.detail"
+              directionReverse
+            ></card>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-7 mb-lg">
+            <!-- line chart -->
+            <div class="card z-index-2">
+              <gradient-line-chart />
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <carousel />
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-lg-7 mb-lg-0 mb-4">
+            <div class="card">
+              <div class="p-3 pb-0 card-header">
+                <div class="d-flex justify-content-between">
+                  <h6 class="mb-2">Team Metrics</h6>
+                </div>
+              </div>
+              <div class="table-responsive">
+                <table class="table align-items-center">
+                  <tbody>
+                    <tr v-for="(sale, index) in sales" :key="index">
+                      <td class="w-30">
+                        <div class="px-2 py-1 d-flex align-items-center">
+                          <div>
+                            <img :src="sale.flag" alt="Country flag" />
+                          </div>
+                          <div class="ms-4">
+                            <p class="mb-0 text-xs font-weight-bold">Name:</p>
+                            <h6 class="mb-0 text-sm">{{ sale.country }}</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <div class="text-center">
+                          <p class="mb-0 text-xs font-weight-bold">Sleep:</p>
+                          <h6 class="mb-0 text-sm">{{ sale.sales }}</h6>
+                        </div>
+                      </td>
+                      <td>
+                        <div class="text-center">
+                          <p class="mb-0 text-xs font-weight-bold">Meditation:</p>
+                          <h6 class="mb-0 text-sm">{{ sale.value }}</h6>
+                        </div>
+                      </td>
+                      <td class="text-sm align-middle">
+                        <div class="text-center col">
+                          <p class="mb-0 text-xs font-weight-bold">Overall Improvement</p>
+                          <h6 class="mb-0 text-sm">{{ sale.bounce }}</h6>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <categories-card />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Card from "@/examples/Cards/Card.vue";
+import GradientLineChart from "@/examples/Charts/GradientLineChart.vue";
+import Carousel from "./components/Carousel.vue";
+import CategoriesCard from "./components/CategoriesCard.vue";
+
+import US from "@/assets/img/icons/flags/US.png";
+import DE from "@/assets/img/icons/flags/DE.png";
+import GB from "@/assets/img/icons/flags/GB.png";
+import BR from "@/assets/img/icons/flags/BR.png";
+
+export default {
+  name: "dashboard-default",
+  data() {
+    return {
+      stats: {
+        money: {
+          title: "Meditation Overview",
+          value: "45 mins Average",
+          percentage: "+55%",
+          iconClass: "ni ni-money-coins",
+          detail: "since Yesterday",
+          iconBackground: "bg-gradient-primary",
+        },
+        users: {
+          title: "Movement",
+          value: "2607 Steps Today",
+          percentage: "+3%",
+          iconClass: "ni ni-world",
+          iconBackground: "bg-gradient-danger",
+          detail: "since Yesterday",
+        },
+        clients: {
+          title: "Recovery",
+          value: "34 Mins Average",
+          percentage: "-2%",
+          iconClass: "ni ni-paper-diploma",
+          percentageColor: "text-danger",
+          iconBackground: "bg-gradient-success",
+          detail: "since last Month",
+        },
+        sales: {
+          title: "Sleep",
+          value: "8 Hours Average",
+          percentage: "+5%",
+          iconClass: "ni ni-cart",
+          iconBackground: "bg-gradient-warning",
+          detail: "than last month",
+        },
+      },
+      sales: {
+        us: {
+          country: "Maria Ligma",
+          sales: "6.5 Hours",
+          value: "1 Hours",
+          bounce: "-29.9%",
+          flag: US,
+        },
+        germany: {
+          country: "John Doe",
+          sales: "7.84 Hours",
+          value: "45 Mins",
+          bounce: "10.22%",
+          flag: DE,
+        },
+        britain: {
+          country: "Jane Judy",
+          sales: "9 Hours",
+          value: "1.1 Hours",
+          bounce: "4.44%",
+          flag: GB,
+        },
+        brasil: {
+          country: "Patrick Bateman",
+          sales: "8 Hours",
+          value: "30 Mins",
+          bounce: "32.14%",
+          flag: BR,
+        },
+      },
+    };
+  },
+  components: {
+    Card,
+    GradientLineChart,
+    Carousel,
+    CategoriesCard,
+  },
+};
+</script>
